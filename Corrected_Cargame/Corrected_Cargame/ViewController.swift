856fbdab3c5369f4e87200f7eadee8f8b66eca0b
sndeathlock <37899763+sndeathlock@users.noreply.github.com>
2018-03-29 16:41:52 +0100

    Add files via upload

---
+++ Corrected_Cargame/Corrected_Cargame/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Corrected_Cargame
+//
+//  Created by sn15abl on 26/03/2018.
+//  Copyright © 2018 sn15abl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var road: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var imageArray: [UIImage]!
+        
+        imageArray = [UIImage(named: "road1.png")!,
+                      UIImage(named: "road2.png")!,
+                      UIImage(named: "road3.png")!,
+                      UIImage(named: "road4.png")!,
+                      UIImage(named: "road5.png")!,
+                      UIImage(named: "road6.png")!,
+                      UIImage(named: "road7.png")!,
+                      UIImage(named: "road8.png")!,
+                      UIImage(named: "road9.png")!,
+                      UIImage(named: "road10.png")!,
+                      UIImage(named: "road11.png")!,
+                      UIImage(named: "road12.png")!,
+                      UIImage(named: "road13.png")!,
+                      UIImage(named: "road14.png")!,
+                      UIImage(named: "road15.png")!,
+                      UIImage(named: "road16.png")!,
+                      UIImage(named: "road17.png")!,
+                      UIImage(named: "road18.png")!,
+                      UIImage(named: "road19.png")!,
+                      UIImage(named: "road20.png")!]
+        
+        road.image = UIImage.animatedImage(with: imageArray, duration: 1.0);
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
